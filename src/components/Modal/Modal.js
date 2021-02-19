7ff4422053e0d5d81cc135840ac3fdf968fafb56
ajaykavalkar15 <ajaykavalkar15@gmail.com>
2021-02-19 17:02:28 +0530

    react animation practice final

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Modal.css";
+import CSSTransition from "react-transition-group/CSSTransition";
+
+const animationTiming = {
+  enter: 500,
+  exit: 1000
+};
+
+function Modal(props) {
+  return (
+    <CSSTransition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={animationTiming}
+      classNames={{
+        enter: '',
+        enterActive: 'ModalOpen',
+        exit: '',
+        exitActive: 'ModalClosed'
+      }}>
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+            </button>
+      </div>
+    </CSSTransition>
+  );
+};
+
+export default Modal;
